7ddfe17ecbfb7f495658f9e6ee7656a4771a4920
Kaif Ahmed <hello@kaif.me>
2018-11-07 12:25:32 +0000

    created and ran seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   valid categories ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:           'Wong Kei',
+    address:        'Chinatown',
+    phone_number:   '0444 555 666',
+    category:       'chinese'
+  },
+  {
+    name:           'Pizza Brixton',
+    address:        'Brixton',
+    phone_number:   '0444 555 666',
+    category:       'italian'
+  },
+  {
+    name:           'Wagamama',
+    address:        'Everywhere',
+    phone_number:   '0444 555 666',
+    category:       'japanese'
+  },
+  {
+    name:           'Morleys',
+    address:        'South London',
+    phone_number:   '0444 555 666',
+    category:       'french'
+  },
+  {
+    name:           'Muscles',
+    address:        'Covent Garden',
+    phone_number:   '0444 555 666',
+    category:       'belgian'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
